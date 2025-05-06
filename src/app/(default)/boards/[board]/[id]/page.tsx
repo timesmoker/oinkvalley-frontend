4403709a1267aa73b7f60d52df43ad8f686ea184
timesmoker <timesmoker3526@gmail.com>
2025-05-06 11:45:17 +0900

    fix : dynamic으로 ssr 방지

--- src/app/(default)/boards/[board]/[id]/page.tsx
+++ src/app/(default)/boards/[board]/[id]/page.tsx
@@ -3,18 +3,21 @@
 import { notFound } from "next/navigation";
 import {boardConfigs} from "@/data/boards/boardConfigs";
 import {PostDetail} from "@/types/posts";
+import dynamic from "next/dynamic";
+
+const Viewer = dynamic(() => import('@/components/boards/Viewer'), { ssr: false });
 
 export default async function PostPage({ params }: {params: { board: string; id: string; } }) {
     const config = boardConfigs[params.board as keyof typeof boardConfigs];
     if (!config) return notFound();
 
     const res = await fetch(
-        `/api/boards/${params.board}/posts/${params.id}`,
-        { cache: "no-store" }
+        `http://nginx/api/boards/${params.board}/posts/${params.id}`,
+        { cache: 'no-store' }
     );
 
     if (!res.ok) {
-        notFound();
+        return notFound();
     }
 
     const post: PostDetail = await res.json();
@@ -23,6 +26,7 @@ export default async function PostPage({ params }: {params: { board: string; id:
         <div className="p-6">
             <h1 className="text-2xl font-bold mb-4">{post.title}</h1>
             <div className="prose">{/* JSON content 렌더링 */}</div>
+            <Viewer content={post.content} />
         </div>
     );
 }
